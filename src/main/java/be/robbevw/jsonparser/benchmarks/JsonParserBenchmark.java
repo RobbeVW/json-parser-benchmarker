a0c637d6280c7491945f189222cfe3a315bd5dc4
Rogier Roex <12000409@student.pxl.be>
2022-04-04 20:14:58 +0200

    json-java branch

--- src/main/java/be/robbevw/jsonparser/benchmarks/JsonParserBenchmark.java
+++ src/main/java/be/robbevw/jsonparser/benchmarks/JsonParserBenchmark.java
@@ -1,8 +1,7 @@
 package be.robbevw.jsonparser.benchmarks;
 
 import be.robbevw.jsonparser.models.Invoice;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import org.json.*;
 import lombok.Getter;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.Level;
@@ -10,8 +9,12 @@ import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
 
-import java.io.File;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 public class JsonParserBenchmark {
@@ -20,21 +23,46 @@ public class JsonParserBenchmark {
     public static class TestData {
 
         @Getter
-        private File invoicesJson;
+        private Path invoicesJson;
 
         @Setup(Level.Trial)
         public void setup() {
-            invoicesJson = new File("src/main/resources/MOCK_DATA.json");
+            invoicesJson = Paths.get("src/main/resources/MOCK_DATA.json");
         }
     }
 
     @State(Scope.Benchmark)
     public static class BenchmarkedObjectMapper {
-        public final ObjectMapper mapper = new ObjectMapper();
+        public List<Invoice> readValue(Path file) throws IOException {
+            List<Invoice> invoices = new ArrayList<>();
+            JSONObject jsonObject;
+
+            BufferedReader reader = Files.newBufferedReader(file);
+            String line;
+            while ((line = reader.readLine()) != null) {
+                jsonObject = new JSONObject(line);
+                invoices.add(jsonToInvoice(jsonObject));
+            }
+            return invoices;
+        }
+
+        //helper method
+        private Invoice jsonToInvoice(JSONObject json) {
+            Invoice invoice = new Invoice();
+            invoice.setTotalAmount(json.getBigDecimal("totalAmount"));
+            invoice.setCompanyName(json.getString("companyName"));
+            invoice.setComment(json.getString("comment"));
+//            invoice.setComment("hello it's me");
+            return invoice;
+        }
+
+        public List<Invoice> init(BenchmarkedObjectMapper objectMapper, TestData testData) throws IOException {
+            return objectMapper.readValue(testData.invoicesJson);
+        }
     }
 
     @Benchmark
     public List<Invoice> init(BenchmarkedObjectMapper objectMapper, TestData testData) throws IOException {
-        return objectMapper.mapper.readValue(testData.invoicesJson, new TypeReference<>() {});
+        return objectMapper.readValue(testData.invoicesJson);
     }
 }
