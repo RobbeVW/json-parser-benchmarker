7f139a76acd70eb7482a94e262f868926ff5ebc5
kristofvanlimbergenPXL <kristof.vanlimbergen@student.pxl.be>
2022-04-09 09:53:26 +0200

    fastJson Benchmark

--- src/main/java/be/robbevw/jsonparser/benchmarks/JsonParserBenchmark.java
+++ src/main/java/be/robbevw/jsonparser/benchmarks/JsonParserBenchmark.java
@@ -1,8 +1,9 @@
 package be.robbevw.jsonparser.benchmarks;
 
 import be.robbevw.jsonparser.models.Invoice;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.alibaba.fastjson.*;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONReader;
 import lombok.Getter;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.Level;
@@ -11,8 +12,14 @@ import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
 
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class JsonParserBenchmark {
 
@@ -30,11 +37,40 @@ public class JsonParserBenchmark {
 
     @State(Scope.Benchmark)
     public static class BenchmarkedObjectMapper {
-        public final ObjectMapper mapper = new ObjectMapper();
+        //public final ObjectMapper mapper = new ObjectMapper();
     }
 
     @Benchmark
-    public List<Invoice> init(BenchmarkedObjectMapper objectMapper, TestData testData) throws IOException {
-        return objectMapper.mapper.readValue(testData.invoicesJson, new TypeReference<>() {});
+    public List<Invoice> init() throws IOException {
+       //optie 1
+        /*
+        List<Invoice> invoices = new ArrayList<Invoice>();
+        File invoiceJson = new File("src/main/resources/MOCK_DATA.json");
+        JSONReader reader = new JSONReader(new FileReader(invoiceJson));
+        reader.startArray();
+        try {
+            while (reader.hasNext()){
+                Invoice in=reader.readObject(Invoice.class);
+                invoices.add(in);
+            }
+            reader.endArray();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        return invoices;
+
+         */
+
+        //optie 2 (iets compacter ook van Mykong :))
+        List<Invoice>invoices = null;
+        try (Stream<String> lines = Files.lines(Paths.get("src/main/resources/MOCK_DATA.json"))) {
+            String content = lines.collect(Collectors.joining());
+            invoices=JSON.parseArray(content,Invoice.class);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        return invoices;
     }
 }
--- src/test/java/be/robbevw/jsonparser/JsonParserApplicationTests.java
+++ src/test/java/be/robbevw/jsonparser/JsonParserApplicationTests.java
@@ -3,6 +3,7 @@ package be.robbevw.jsonparser;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
 class JsonParserApplicationTests {
 
