bd946560d631ba4ef100e8510d258b088a44e1d1
Rogier Roex <12000409@student.pxl.be>
2022-04-14 20:09:28 +0200

    UPDATE fast json to new functionality

--- src/main/java/be/robbevw/jsonparser/benchmarks/JsonParserBenchmark.java
+++ src/main/java/be/robbevw/jsonparser/benchmarks/JsonParserBenchmark.java
@@ -1,76 +1,105 @@
 package be.robbevw.jsonparser.benchmarks;
 
 import be.robbevw.jsonparser.models.Invoice;
-import com.alibaba.fastjson.*;
 import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONReader;
-import lombok.Getter;
-import org.openjdk.jmh.annotations.Benchmark;
-import org.openjdk.jmh.annotations.Level;
-import org.openjdk.jmh.annotations.Scope;
-import org.openjdk.jmh.annotations.Setup;
-import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.*;
 
-import java.io.File;
-import java.io.FileReader;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
+@State(Scope.Benchmark)
 public class JsonParserBenchmark {
 
-    @State(Scope.Benchmark)
-    public static class TestData {
+    private List<String> testData;
 
-        @Getter
-        private File invoicesJson;
+    @Setup
+    public void setUp(){
+        testData = getTestData();
+    }
 
-        @Setup(Level.Trial)
-        public void setup() {
-            invoicesJson = new File("src/main/resources/MOCK_DATA.json");
+    /**
+     * https://www.baeldung.com/java-microbenchmark-harness
+     * 6. Dead Code Elimination
+     * return List bij Benchmarks is ivm invloed op test results..
+     * returnType VOID zorgt voor dat compiler gaat optimisen en dat beinvloedt het resultaat.
+     *
+     * Benchmarkmodes javadocs:
+     * http://javadox.com/org.openjdk.jmh/jmh-core/1.7/org/openjdk/jmh/annotations/Mode.html
+     * - Average time: average time per per operation.
+     * - Single shot time: measures the time for a single operation.
+     * - Throughput: operations per unit of time.
+     * - Sample time: samples the time for each operation.
+     */
+    @Fork(value = 1, warmups = 1)
+    @Benchmark()
+    @BenchmarkMode(Mode.AverageTime)
+    public List<Invoice> averageTime(){
+        List<Invoice> invoices = new ArrayList<>();
+        for (String line : testData){
+            invoices.add(jsonToInvoice(line));
         }
+        return invoices;
     }
 
-    @State(Scope.Benchmark)
-    public static class BenchmarkedObjectMapper {
-        //public final ObjectMapper mapper = new ObjectMapper();
-    }
-
+    @Fork(value = 1, warmups = 1)
     @Benchmark
-    public List<Invoice> init() throws IOException {
-       //optie 1
-        /*
-        List<Invoice> invoices = new ArrayList<Invoice>();
-        File invoiceJson = new File("src/main/resources/MOCK_DATA.json");
-        JSONReader reader = new JSONReader(new FileReader(invoiceJson));
-        reader.startArray();
-        try {
-            while (reader.hasNext()){
-                Invoice in=reader.readObject(Invoice.class);
-                invoices.add(in);
-            }
-            reader.endArray();
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
-        return invoices;
-
-         */
-
-        //optie 2 (iets compacter ook van Mykong :))
-        List<Invoice>invoices = null;
-        try (Stream<String> lines = Files.lines(Paths.get("src/main/resources/MOCK_DATA.json"))) {
-            String content = lines.collect(Collectors.joining());
-            invoices=JSON.parseArray(content,Invoice.class);
-        }
-        catch (Exception e){
-            e.printStackTrace();
+    @BenchmarkMode(Mode.SingleShotTime)
+    public List<Invoice> singleShotTime() {
+        List<Invoice> invoices = new ArrayList<>();
+        for (String line : testData) {
+            invoices.add(jsonToInvoice(line));
         }
         return invoices;
     }
+
+    @Fork(value = 1, warmups = 1)
+    @Benchmark
+    @BenchmarkMode(Mode.Throughput)
+    public List<Invoice> throughPut() {
+        List<Invoice> invoices = new ArrayList<>();
+        for (String line : testData) {
+            invoices.add(jsonToInvoice(line));
+        }
+        return invoices;
+    }
+
+    @Fork(value = 1, warmups = 1)
+    @Benchmark
+    @BenchmarkMode(Mode.SampleTime)
+    public List<Invoice> sampleTime() {
+        List<Invoice> invoices = new ArrayList<>();
+        for (String line : testData) {
+            invoices.add(jsonToInvoice(line));
+        }
+        return invoices;
+    }
+
+    //JsonParser implementation
+    private Invoice jsonToInvoice(String line) {
+        // fast-json
+        return JSON.parseObject(line, Invoice.class);
+    }
+
+    //#region helper method
+    public List<String> getTestData() {
+        List<String> testData = new ArrayList<>();
+
+        try (BufferedReader reader = Files.newBufferedReader(Paths.get("src/main/resources/MOCK_DATA.json"))) {
+            String line;
+            int counter = 0;
+            while ((line = reader.readLine()) != null) {
+                counter++;
+                if (counter != 1000) { line = line.substring(0, line.length() -1); }
+                testData.add(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return testData;
+    }
+//#endregion
 }
